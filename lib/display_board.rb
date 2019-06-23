d81972e9d6416af4b2a0f9598df0edcde7e221b9
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-06-23 18:11:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -12,4 +13,14 @@ def display_board(board)
 end
 
 display_board(board)
+=======
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
+>>>>>>> 2e1389cde9ef78cb3bb654630d1a1e4a3c99c27f
 
